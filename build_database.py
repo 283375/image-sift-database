29afcc4e9535975f69f635b63cf311565be86ee0
283375 <bilibili283375@163.com>
2023-08-10 23:07:27 +0800

    refacor!: drop json support

--- build_database.py
+++ build_database.py
@@ -1,5 +1,3 @@
-import base64
-import json
 import os
 from pathlib import Path
 import sqlite3
@@ -53,37 +51,20 @@ class SIFTDatabase:
             p for p in self.images_path.glob("**/*") if p.suffix.lower() in self.EXTS
         ]
 
-        if self.output_file.endswith(".json"):
-            output_dict = {}
+        conn = sqlite3.connect(self.output_file)
+        conn.execute("PRAGMA journal_mode=WAL;")
+        with conn:
+            conn.execute("CREATE TABLE sift (id TEXT PRIMARY KEY, descriptors BLOB)")
+            cursor = conn.cursor()
+            insert_batch: list[tuple[str, bytes]] = []
             for image_path in tqdm(images):
                 try:
                     img_gray = cv2.imread(str(image_path), cv2.IMREAD_GRAYSCALE)
                     _, descriptors = self.sift_detectAndCompute(img_gray, SZ)
-                    output_dict[image_path.stem] = base64.b64encode(
-                        descriptors.tobytes()
-                    ).decode("utf-8")
+                    insert_batch.append((image_path.stem, descriptors.tobytes()))
                 except Exception as e:
                     tqdm.write("".join(traceback.format_exception(e)))
-
-            with open(self.output_file, "w", encoding="utf-8") as output_f:
-                json.dump(output_dict, output_f, ensure_ascii=False)
-        elif self.output_file.endswith(".db"):
-            conn = sqlite3.connect(self.output_file)
-            conn.execute("PRAGMA journal_mode=WAL;")
-            with conn:
-                conn.execute(
-                    "CREATE TABLE sift (id TEXT PRIMARY KEY, descriptors BLOB)"
-                )
-                cursor = conn.cursor()
-                insert_batch: list[tuple[str, bytes]] = []
-                for image_path in tqdm(images):
-                    try:
-                        img_gray = cv2.imread(str(image_path), cv2.IMREAD_GRAYSCALE)
-                        _, descriptors = self.sift_detectAndCompute(img_gray, SZ)
-                        insert_batch.append((image_path.stem, descriptors.tobytes()))
-                    except Exception as e:
-                        tqdm.write("".join(traceback.format_exception(e)))
-                cursor.executemany(
-                    "INSERT INTO sift (id, descriptors) VALUES (?, ?)", insert_batch
-                )
-                conn.commit()
+            cursor.executemany(
+                "INSERT INTO sift (id, descriptors) VALUES (?, ?)", insert_batch
+            )
+            conn.commit()
